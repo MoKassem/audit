bbe9030a54c83993e961fd0ad66914eb600d79b7
mokassem <mohamed.kassem@usask.ca>
2019-07-18 09:06:58 -0400

    test patching namespace and releaseNamespace with the GENERATED_NAMESPACE

---
+++ ci/scripts/build-apply-component.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+source ci/scripts/common-func.sh
+
+REQUIRED_ENV_VARS=(GENERATED_NAMESPACE)
+
+export_keyval_env
+
+check_req_env_vars
+
+setup_kubectl_context
+
+[ -d ci/configs/${TARGET_PLATFORM} ] && cd ci/${TARGET_PLATFORM/values}
+## Substitute the GENERATED_NAMESPACE in the releaseNamespace.yaml patch
+NAMESPACE_PATCH=$(cat releaseNamespace.yaml | envsubst) && echo ${NAMESPACE_PATCH} > releaseNamespace.yaml
+
+cd ..
+yq w -i kustomization.yaml namespace ${GENERATED_NAMESPACE}
+kustomize build . --enable_alpha_plugins | kubectl apply -f -
